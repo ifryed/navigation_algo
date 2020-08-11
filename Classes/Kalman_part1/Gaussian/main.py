a27c4d742e21ca6aef66a4e342f1c8c8a3aa2394
Shai <ifryed@gmail.com>
2020-08-11 09:04:52 +0300

    Added the questions to Gauss main

--- Classes/Kalman_part1/Gaussian/main.py
+++ Classes/Kalman_part1/Gaussian/main.py
@@ -11,15 +11,15 @@ import math
 
 def main():
     # we print a heading ComputeGausianand make it bigger using HTML formatting
-    print(ComputeGausian(8,2,8))
-    print(ComputeGausian(8,10,8))
+    print(ComputeGausian(8, 2, 8))
+    print(ComputeGausian(8, 10, 8))
     print(ComputeGausian(10, 1.2, 10))
 
     print("{:.6},{:.4}".format(*predict(5, 0, 15, 2)))
     print("{:.2f},{:.4f}".format(*predict(5, 1000, 15, 2)))
 
-    print("{:.3}".format(GaussPDF(100,15,125)))
-    print("{:.3}".format(GaussPDF(115,20,125)))
+    print("{:.3}".format(GaussPDF(100, 15, 125)))
+    print("{:.3}".format(GaussPDF(115, 20, 125)))
 
 
 if __name__ == '__main__':
