9f111fdb829db84e06e591d9112f04250070d91e
Shai <ifryed@gmail.com>
2020-08-28 15:57:21 +0300

    fixed a stupid bug

--- Exercises/ex3_kalman_advanced/src/main.py
+++ Exercises/ex3_kalman_advanced/src/main.py
@@ -20,7 +20,7 @@ def calF(state: np.ndarray, dt: float = 1.0) -> np.ndarray:
 
 
 def kalmanFilter(state: np.ndarray, P: np.ndarray, R: np.ndarray,
-                 F: np.ndarray, H: np.ndarray, measurment: np.ndarray, dt: float = 1) -> (np.ndarray, np.ndarray):
+                 F: np.ndarray, H: np.ndarray, measurment: np.ndarray) -> (np.ndarray, np.ndarray):
     # Stage I: Predict
     x = F.dot(state)
     P = F.dot(P.dot(F.T))
@@ -33,7 +33,7 @@ def kalmanFilter(state: np.ndarray, P: np.ndarray, R: np.ndarray,
     )
 
     state_out = x + K.dot(measurment - H.dot(x))
-    P_out = (1 - K.dot(H)).dot(P) * 2
+    P_out = (np.eye(len(x)) - K.dot(H)).dot(P)
 
     return state_out, P_out
 
@@ -42,8 +42,9 @@ def main():
     init_state = np.array([[5,  # Pos X
                             5,  # Pos Y
                             0,  # Vel X
-                            0]]).T  # Vel y
-    init_p = np.diag([12, 12, 12, 12])
+                            0,  # Vel y
+                            ]]).T
+    init_p = np.diag([12] * len(init_state))
     F = lambda dt: np.array([
         [1, 0, dt, 0],
         [0, 1, 0, dt],
@@ -54,7 +55,7 @@ def main():
     H = np.array([
         [1, 0, 0, 0],
         [0, 1, 0, 0],
-    ])  # TODO: figure out the matrix
+    ])
 
     # Read data
     # Reading the data file, while skipping the Radar data lines and setting the headers
@@ -65,6 +66,7 @@ def main():
     state = init_state.copy()
 
     last_dt = data.iloc[0]['timestamp'] - MS2SEC
+    error_log = []
     for data_line in data.iterrows():
         meas = np.array([[
             data_line[1]['x_measured'],
@@ -78,14 +80,16 @@ def main():
         last_dt = data_line[1]['timestamp']
 
         state, new_p = kalmanFilter(state, new_p, R, F(dt), H, meas)
-
+        rmse = RMSE(state[:2], gt[:2])
+        error_log.append(rmse)
         plt.plot(gt[0], gt[1], 'g*')
         plt.plot(meas[0], meas[1], 'ro')
         plt.plot(state[0], state[1], 'b*')
         plt.legend(['GT', 'Meas', 'KF'], loc=2)
-        plt.title("RMSE {:.3f}".format(RMSE(state[:2], gt[:2])))
-        plt.pause(.1)
+        plt.title("RMSE {:.3f}".format(rmse))
+        plt.pause(.01)
 
+    print("Mean RMSE: {:3f}".format(np.array(error_log).mean()))
     plt.show()
 
 
