6a2ccadfb88917ceaa95195e574aa14f7974edb1
Shai <ifryed@gmail.com>
2020-08-29 22:17:59 +0300

    Made EKF into a class

---
+++ Exercises/ex3_kalman_advanced/src/kalmanLib.py
@@ -0,0 +1,43 @@
+import numpy as np
+from typing import Callable
+
+
+class ExtendtedKalmanFilter:
+    def __init__(self, init_state: np.ndarray, init_p: np.ndarray, R: np.ndarray, H: np.ndarray):
+        self.state = init_state
+        self.P = init_p
+        self.R = R
+        self.H = H
+        self.I = np.eye(len(self.state))
+
+    def _extractState(self) -> (float, float, float, float, float, float):
+        x, y, vx, vy = self.state.flatten()
+        r = np.sqrt(np.square(x) + np.square(y))
+        h = np.arctan2(vy, vx)
+        return x, y, vx, vy, r, h
+
+    def _createFMatrix(self, dt: float) -> np.ndarray:
+        f_mat = np.array([
+            [1, 0, dt, 0],
+            [0, 1, 0, dt],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ])
+        return f_mat
+
+    def predict(self, dt: float):
+        f_mat = self._createFMatrix(dt)
+        self.state = f_mat.dot(self.state)
+        self.P = f_mat.dot(self.P).dot(f_mat.T)
+
+    def update(self, measurment: np.ndarray):
+        K = self.P.dot(self.H.T).dot(
+            np.linalg.pinv(
+                self.H.dot(self.P.dot(self.H.T)) + self.R
+            )
+        )
+
+        self.state = self.state + K.dot(measurment - self.H.dot(self.state))
+        self.P = (self.I - K.dot(self.H)).dot(self.P)
+
+        return self.state, self.P
--- Exercises/ex3_kalman_advanced/src/main.py
+++ Exercises/ex3_kalman_advanced/src/main.py
@@ -1,41 +1,44 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from kalmanLib import ExtendtedKalmanFilter
+import time
 
 MS2SEC = 100000
+MS2MIN = 60 * MS2SEC
 DATA_PATH = '../data/obj_pose-laser-radar-synthetic-input.txt'
-LIDAR_HEADER = 'sensor_type,x_measured,y_measured,timestamp,' \
-               'x_groundtruth,y_groundtruth,vx_groundtruth,' \
-               'vy_groundtruth,yaw_groundtruth,yawrate_groundtruth'.split(',')
+LIDAR_HEADERS = 'sensor_type,x_measured,y_measured,timestamp,' \
+                'x_groundtruth,y_groundtruth,vx_groundtruth,' \
+                'vy_groundtruth,yaw_groundtruth,yawrate_groundtruth'.split(',')
 
 
 def RMSE(a, b):
     return np.sqrt(np.square(a - b).mean())
 
 
-def calF(state: np.ndarray, dt: float = 1.0) -> np.ndarray:
-    x, y, vx, vy = state.flatten()
-    heading = np.arctan2(vy, vx)
-    d = np.sqrt(np.square(x) + np.square(y))
+def updatePlot(gt: np.ndarray, meas: np.ndarray, state: np.ndarray, rmse: float) -> None:
+    # Plot
+    plt.plot(gt[0], gt[1], 'g*')
+    plt.plot(meas[0], meas[1], 'ro')
+    plt.plot(state[0], state[1], 'b*')
+    plt.legend(['GT', 'Meas', 'KF'], loc=2)
+    plt.title("RMSE {:.3f}".format(rmse))
+    plt.pause(.01)
 
 
-def kalmanFilter(state: np.ndarray, P: np.ndarray, R: np.ndarray,
-                 F: np.ndarray, H: np.ndarray, measurment: np.ndarray) -> (np.ndarray, np.ndarray):
-    # Stage I: Predict
-    x = F.dot(state)
-    P = F.dot(P.dot(F.T))
+def extractDataLine(data_line: pd.DataFrame) -> (np.ndarray, np.ndarray, float):
+    meas = np.array([[
+        data_line[1]['x_measured'],
+        data_line[1]['y_measured']]]).T
+    gt = np.array([[
+        data_line[1]['x_groundtruth'],
+        data_line[1]['y_groundtruth']]]).T
 
-    # Stage II: Update
-    K = P.dot(H.T).dot(
-        np.linalg.pinv(
-            H.dot(P.dot(H.T)) + R
-        )
-    )
+    time_stamp = data_line[1]['timestamp']
 
-    state_out = x + K.dot(measurment - H.dot(x))
-    P_out = (np.eye(len(x)) - K.dot(H)).dot(P)
-
-    return state_out, P_out
+    return meas, gt, time_stamp
 
 
 def main():
@@ -45,50 +48,44 @@ def main():
                             0,  # Vel y
                             ]]).T
     init_p = np.diag([12] * len(init_state))
-    F = lambda dt: np.array([
-        [1, 0, dt, 0],
-        [0, 1, 0, dt],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1],
-    ])
     R = np.diag([0.0255, 0.0255])
     H = np.array([
         [1, 0, 0, 0],
-        [0, 1, 0, 0],
+        [0, 1, 0, 0]
     ])
 
     # Read data
     # Reading the data file, while skipping the Radar data lines and setting the headers
-    data = pd.read_csv(DATA_PATH, sep='\t', header=None, comment='R', names=LIDAR_HEADER)
-    print(data.head())
+    data = pd.read_csv(DATA_PATH, sep='\t', header=None, comment='R', names=LIDAR_HEADERS)
+    print("Data sample\n", data.head())
 
-    new_p = init_p.copy()
-    state = init_state.copy()
+    ekf = ExtendtedKalmanFilter(init_state, init_p, R, H)
 
     last_dt = data.iloc[0]['timestamp'] - MS2SEC
+
+    os.makedirs('../out', exist_ok=True)
+    logger = open('../out/log_{}.txt'.format(np.floor(time.time()).astype(int)), 'w')
+
     error_log = []
-    for data_line in data.iterrows():
-        meas = np.array([[
-            data_line[1]['x_measured'],
-            data_line[1]['y_measured']]]).T
-        gt = np.array([[
-            data_line[1]['x_groundtruth'],
-            data_line[1]['y_groundtruth']]]).T
+    for i, data_line in enumerate(data.iterrows()):
+        meas, gt, time_stamp = extractDataLine(data_line)
 
-        dt = data_line[1]['timestamp'] - last_dt
-        dt /= MS2SEC
-        last_dt = data_line[1]['timestamp']
+        dt = (time_stamp - last_dt) / MS2MIN
+        last_dt = time_stamp
 
-        state, new_p = kalmanFilter(state, new_p, R, F(dt), H, meas)
+        ekf.predict(dt)
+        state, new_p = ekf.update(meas)
+
         rmse = RMSE(state[:2], gt[:2])
         error_log.append(rmse)
-        plt.plot(gt[0], gt[1], 'g*')
-        plt.plot(meas[0], meas[1], 'ro')
-        plt.plot(state[0], state[1], 'b*')
-        plt.legend(['GT', 'Meas', 'KF'], loc=2)
-        plt.title("RMSE {:.3f}".format(rmse))
-        plt.pause(.01)
+        logger.write('{}:{:3f}\n'.format(i, rmse))
 
+        if i%5 ==0:
+            logger.flush()
+
+        updatePlot(gt, meas, state, rmse)
+
+    logger.close()
     print("Mean RMSE: {:3f}".format(np.array(error_log).mean()))
     plt.show()
 
